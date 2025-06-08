4d463f55dc1da3aa5515c1a2d4f7c9caab730a4f
Asmaakhaled27 <asmaakhaled9298@gmail.com>
2025-06-08 17:36:43 +0300

    add server loginpage

--- ZeroGravityZone/src/App.jsx
+++ ZeroGravityZone/src/App.jsx
@@ -1,16 +1,20 @@
-
+import React from 'react';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import LoginPage from './component/LoginPage.jsx';
+import { Box } from '@mui/material';
+// import axios from 'axios';
 
 function App() {
- 
+  
 
   return (
-    <>
-      <div>
-       
-      </div>
-     
-    </>
-  )
+    <Box>
+      <LoginPage />
+    </Box>
+  );
 }
 
-export default App
+export default App;
